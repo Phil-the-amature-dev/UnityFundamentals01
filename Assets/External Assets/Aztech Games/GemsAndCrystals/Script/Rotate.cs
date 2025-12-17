d00c5dbbcaea0ab2e33990bcf866527b5807f616
Phil-the-amature-dev <98315948+Phil-the-amature-dev@users.noreply.github.com>
2025-12-17 12:19:01 +0100

    Main menu WIP

--- Assets/External Assets/Aztech Games/GemsAndCrystals/Script/Rotate.cs
+++ Assets/External Assets/Aztech Games/GemsAndCrystals/Script/Rotate.cs
@@ -5,6 +5,6 @@ namespace AztechGames
 public class Rotate : MonoBehaviour
 {
     public float speed = 10f; // Speed of rotation in degrees per second
-    void Update() { transform.Rotate(Vector3.up, speed * Time.deltaTime); } // Rotate around the Y-axis
+    void Update() { transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.Self); } // Rotate around the Y-axis
 }
 }
